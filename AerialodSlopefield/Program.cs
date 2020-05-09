29a756c54c9b4e4086ffd0501060082112fe7f48
Bogdan Stancescu <bogdan@moongate.ro>
2020-05-09 16:14:52 +0300

    Added first iteration of CLI parameters

--- AerialodSlopefield/Program.cs
+++ AerialodSlopefield/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 
 namespace AerialodSlopefield
 {
     class Program
     {
+        public class Options
+        {
+            [Option("minx", Required =true, HelpText ="The minimum X value to render.")]
+            public float MinX { get; set; }
+
+            [Option("maxx", Required = true, HelpText = "The maximum X value to render.")]
+            public float MaxX { get; set; }
+
+            [Option("stepx", Required = true, HelpText = "The step for X values.")]
+            public float StepX { get; set; }
+
+            [Option("miny", Required = true, HelpText = "The minimum Y value to render.")]
+            public float MinY { get; set; }
+
+            [Option("maxy", Required = true, HelpText = "The maximum Y value to render.")]
+            public float MaxY { get; set; }
+
+            [Option("stepy", Required = true, HelpText = "The step for Y values.")]
+            public float StepY { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o => GenerateSlopeField(o));
+        }
+
+        private static void GenerateSlopeField(Options o)
+        {
+            throw new NotImplementedException();
         }
     }
 }
